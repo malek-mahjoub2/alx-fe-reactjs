1d002728962456ae4425acdf792d04d23f9f5924
malek mahjoub <malekmahjoub80@gmail.com>
2024-11-01 23:56:19 -1100

    Your commit message here

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <WelcomeMessage />
+            <MainContent />
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
