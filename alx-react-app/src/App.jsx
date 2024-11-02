81ac54ed23ce66318f8fdf1d942049506a59cc20
malek mahjoub <malekmahjoub80@gmail.com>
2024-11-02 07:53:48 -1100

    import the header file in app.jsk

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Header from './Header';
+import Header from './components/Header';
 import WelcomeMessage from './components/WelcomeMessage';
 import MainContent from './MainContent';
 import Footer from './Footer';
