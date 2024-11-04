89aa063d6921ea8377fa6ae5749ad1c746b2a045
malek mahjoub <malekmahjoub80@gmail.com>
2024-11-04 08:51:24 -1100

    add changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import UserProfile from './components/UserProfile';
 import Header from './components/Header';
-import WelcomeMessage from './components/WelcomeMessage';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 
 const App = () => {
   const userData = {
@@ -14,14 +14,15 @@ const App = () => {
 
   return (
     <div className="App">
-       <UserProfile
+        <UserProfile
             name="Alice"
             age={25}
             bio="Loves hiking and photography"
         />
       <Header />
-      <WelcomeMessage />
+      
       <MainContent />
+      <WelcomeMessage />
       <Footer />
     </div>
   );
