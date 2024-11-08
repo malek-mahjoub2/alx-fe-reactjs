d9d7163041c40378daa63b06422e455daaffa623
malek mahjoub <malekmahjoub80@gmail.com>
2024-11-08 11:04:33 -1100

    adjust the inputs

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import UserInfo from '.components/UserInfo';
+import React, { useContext } from 'react';
+import UserContext from '../UserContext'; // Import UserContext
 
 function UserProfile() {
-  return <UserInfo />;
+  // Use useContext to get the userData from UserContext
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
 export default UserProfile;
