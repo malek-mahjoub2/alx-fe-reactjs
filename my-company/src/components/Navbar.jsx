b68b9d72b630b9a8793c534a0890ed5bf6f217a1
malek mahjoub <malekmahjoub80@gmail.com>
2024-11-08 11:32:07 -1100

    add astyling to navBar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';  // Import the CSS file
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <ul className="navList">
-        <li className="navItem">
-          <Link to="/">Home</Link>
+    <nav style={{ backgroundColor: '#333', padding: '10px' }}>
+      <ul style={{ display: 'flex', justifyContent: 'space-between', listStyleType: 'none', margin: 0, padding: 0 }}>
+        <li style={{ color: 'white', fontSize: '18px' }}>
+          <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
         </li>
-        <li className="navItem">
-          <Link to="/about">About</Link>
+        <li style={{ color: 'white', fontSize: '18px' }}>
+          <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
         </li>
-        <li className="navItem">
-          <Link to="/services">Services</Link>
+        <li style={{ color: 'white', fontSize: '18px' }}>
+          <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
         </li>
-        <li className="navItem">
-          <Link to="/contact">Contact</Link>
+        <li style={{ color: 'white', fontSize: '18px' }}>
+          <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
         </li>
       </ul>
     </nav>
   );
 }
+
 export default Navbar;
