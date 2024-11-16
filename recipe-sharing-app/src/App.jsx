b855c5bc3d53e1bb239677e796fe5afaa6c068df
malek mahjoub <malekmahjoub80@gmail.com>
2024-11-16 23:13:57 +0100

    Add delete buttom

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Import necessary routing components
+import RecipeList from './components/RecipeList';  // Import RecipeList component
+import RecipeDetails from './components/RecipeDetails';  // Import RecipeDetails component
+import AddRecipeForm from './components/AddRecipeForm';  // Import AddRecipeForm component
+import EditRecipeForm from './components/EditRecipeForm';  // Import EditRecipeForm component
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />  {/* Component for adding recipes */}
-      <RecipeList />  {/* Component for displaying the list of recipes */}
-    </div>
+    <Router>  {/* This wraps the entire application in a Router */}
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes>  {/* Define all routes inside Routes */}
+          {/* Define the home page route */}
+          <Route path="/" element={<RecipeList />} />
+
+          {/* Define route for adding a new recipe */}
+          <Route path="/add" element={<AddRecipeForm />} />
+
+          {/* Define route for editing a recipe */}
+          <Route path="/edit/:id" element={<EditRecipeForm />} />
+
+          {/* Define route for viewing recipe details */}
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
