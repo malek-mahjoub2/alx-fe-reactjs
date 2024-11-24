56c3c85e67abab356095cfb20c523f2fd234ea05
malek mahjoub <malekmahjoub80@gmail.com>
2024-11-24 18:24:01 +0100

    upgrade profile

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+
+// Define the nested components
+const ProfileDetails = () => {
+  return (
+    <div>
+      <h3>Profile Details</h3>
+      {/* Add your profile details content here */}
+    </div>
+  );
+};
+
+const ProfileSettings = () => {
+  return (
+    <div>
+      <h3>Profile Settings</h3>
+      {/* Add your profile settings content here */}
+    </div>
+  );
+};
 
 const Profile = () => {
   return (
@@ -9,7 +28,10 @@ const Profile = () => {
         <Link to="details">Profile Details</Link>
         <Link to="settings">Profile Settings</Link>
       </nav>
-      <Outlet /> {/* This will render nested components */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 };
