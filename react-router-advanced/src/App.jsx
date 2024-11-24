c084726f0953fa54e29c8d7ae0cf39858195295d
malek mahjoub <malekmahjoub80@gmail.com>
2024-11-24 18:27:00 +0100

    fix the app.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import ProfileDetails from './pages/ProfileDetails';
@@ -7,6 +8,7 @@ import ProfileSettings from './pages/ProfileSettings';
 import Post from './pages/Post';
 import NotFound from './pages/NotFound';
 import ProtectedRoute from './components/ProtectedRoute';
+import BlogPost from './pages/BlogPost'; // Import the BlogPost component
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -28,6 +30,7 @@ const App = () => {
           <Route path="settings" element={<ProfileSettings />} />
         </Route>
         <Route path="/post/:postId" element={<Post />} />
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Add the BlogPost route */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
