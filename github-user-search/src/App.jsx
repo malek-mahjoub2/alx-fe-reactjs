cbb83c6730781cea618682464a0aeff964204898
malek-mahjoub2 <malekmahjoub80@gmail.com>
2024-12-15 13:43:14 +0100

    import the search in App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,20 @@
 
 import React from "react";
 import "./App.css";
-
+import Search from './components/Search';
+import './styles/tailwind.css';
+import Search from './Search'; 
+const myData = [
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+  // ... more items
+];
 function App() {
   return (
     <div className="App">
       <h1>GitHub User Search</h1>
-      {/* You can add components like SearchBar here */}
+      <Search data={myData} /> 
     </div>
   );
 }
